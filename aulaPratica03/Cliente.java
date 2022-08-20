1aaf5ebf6e279d0cfaaefa85d7f2d75ce158431e
Bernardo Busatto <berbusatto@gmail.com>
2022-08-20 15:10:02 -0300

    aula pratica 03

---
+++ aulaPratica03/Cliente.java
@@ -0,0 +1,50 @@
+package aulaPratica03;
+
+public class Cliente {
+    private String cpf;
+    private String nome;
+    private String email;
+    private int anoNascimento;
+
+    @Override
+    public String toString() {
+        return "Cliente {" +
+                "cpf='" + cpf + '\'' +
+                ", nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", anoNascimento=" + anoNascimento +
+                '}';
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAnoNascimento() {
+        return anoNascimento;
+    }
+
+    public void setAnoNascimento(int anoNascimento) {
+        this.anoNascimento = anoNascimento;
+    }
+}
