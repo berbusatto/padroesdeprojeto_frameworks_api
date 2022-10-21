fcf0ac818f2037c7c3e068b80a59c6620af70f7c
Bernardo Busatto <berbusatto@gmail.com>
2022-10-21 14:26:32 -0300

    trabalho junit

--- trabalhoJUnit/src/main/java/ArrayUtil.java
+++ trabalhoJUnit/src/main/java/ArrayUtil.java
@@ -3,58 +3,60 @@ import static java.lang.Integer.MIN_VALUE;
 
 public class ArrayUtil {
 
-    public static int soma(int vetor[]){
+    public static int soma(int[] vetor){
         int resultado = 0;
-        for(int i = 0; i < vetor.length; i++){
-            resultado += vetor[i];
+        for (int j : vetor) {
+            resultado += j;
         }
         return resultado;
     }
-    public static int menor(int vetor[]){
+    public static int menor(int[] vetor){
         int menor = MAX_VALUE;
-        for (int i = 0; i < vetor.length; i++){
-            if(vetor[i]< menor){
-                menor = vetor[i];
+        for (int j : vetor) {
+            if (j < menor) {
+                menor = j;
             }
         }
         return menor;
     }
 
-    public static int maior(int vetor[]){
+    public static int maior(int[] vetor){
         int maior = MIN_VALUE;
-        for (int i = 0; i < vetor.length; i++){
-            if(vetor[i] > maior){
-                maior = vetor[i];
+        for (int j : vetor) {
+            if (j > maior) {
+                maior = j;
             }
         }
         return maior;
     }
 
-    public static int negativo(int vetor[]){
+    public static int negativo(int[] vetor){
         int qtdNegativos = 0;
-        for (int i = 0; i < vetor.length; i++){
-            if(vetor[i] < 0){
+        for (int j : vetor) {
+            if (j < 0) {
                 qtdNegativos++;
             }
         }
         return qtdNegativos;
     }
 
-    public static boolean par(int vetor[]){
+    public static boolean par(int[] vetor){
         boolean par = true;
-        for(int i = 0; i < vetor.length; i++){
-            if (vetor[i] %2 != 0){
+        for (int j : vetor) {
+            if (j % 2 != 0) {
                 par = false;
+                break;
             }
         }
         return par;
     }
 
-    public static int busca(int vetor[], double alvo){
-        int posicao;
+    public static int busca(int[] vetor, double alvo){
         for (int i = 0; i < vetor.length; i++){
-            if
+            if (vetor[i] == alvo) {
+                return i;
+            }
         }
+        return -1;
     }
-
 }
--- trabalhoJUnit/src/test/java/ArrayUtilTest.java
+++ trabalhoJUnit/src/test/java/ArrayUtilTest.java
@@ -8,39 +8,36 @@ class ArrayUtilTest {
     @Test
     void soma() {
         int[] valores = {1,5,10,30};
-        Assert.assertEquals(46, ArrayUtil.soma(valores));
+        assertEquals(46, ArrayUtil.soma(valores));
     }
 
     @Test
     void menor() {
         int[] valores = {91,5,123,19};
-        Assert.assertEquals(5, ArrayUtil.menor(valores));
+        assertEquals(5, ArrayUtil.menor(valores));
     }
 
     @Test
     void maior() {
         int[] valores = {91,5,123,19};
-        Assert.assertEquals(123, ArrayUtil.maior(valores));
+        assertEquals(123, ArrayUtil.maior(valores));
     }
 
 
     @Test
     void negativo() {
         int[] valoresA = {91,5,123,19};
-        Assert.assertEquals(0,ArrayUtil.negativo(valoresA));
+        assertEquals(0, ArrayUtil.negativo(valoresA));
 
         int[] valoresB = {10, -5, 35, 42, 54};
-        Assert.assertEquals(1,ArrayUtil.negativo(valoresB));
+        assertEquals(1, ArrayUtil.negativo(valoresB));
 
         int[] valoresC = {1,4,-78,90,-89, 99};
-        Assert.assertEquals(2,ArrayUtil.negativo(valoresC));
+        assertEquals(2, ArrayUtil.negativo(valoresC));
 
         int[] valoresD = {-2, -7, -10, -20, -40};
-        Assert.assertEquals(5,ArrayUtil.negativo(valoresD));
-
-
+        assertEquals(5, ArrayUtil.negativo(valoresD));
     }
-
 
     @Test
     void par() {
@@ -52,5 +49,20 @@ class ArrayUtilTest {
 
         int[] valoresC = {1,3,5};
         assertFalse(ArrayUtil.par(valoresC));
+    }
+
+    @Test
+    void busca(){
+        int[] valoresA = {2,4,6,8};
+        assertEquals(3, ArrayUtil.busca(valoresA, 8));
+        assertEquals(-1, ArrayUtil.busca(valoresA, 5));
+
+        int[] valoresB = {10,15,20,42,54};
+        assertEquals(0, ArrayUtil.busca(valoresB, 10));
+        assertEquals(-1, ArrayUtil.busca(valoresB, 25));
+
+        int[] valoresC = {1, -3, 5};
+        assertEquals(-1, ArrayUtil.busca(valoresC, -1));
+        assertEquals(0, ArrayUtil.busca(valoresC, 1));
     }
 }
