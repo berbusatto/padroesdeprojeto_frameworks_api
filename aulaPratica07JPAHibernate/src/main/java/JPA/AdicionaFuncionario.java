83ac6b8728f4afe55eac7dfad48cb828c3a5ac67
Bernardo Busatto <berbusatto@gmail.com>
2022-11-04 22:25:35 -0300

    modelando BD com hibernate

---
+++ aulaPratica07JPAHibernate/src/main/java/JPA/AdicionaFuncionario.java
@@ -0,0 +1,39 @@
+package JPA;
+
+import controllers.TransactionController;
+import modelo.Tarefa;
+import modelo2.ClienteEmpresa;
+import modelo2.Funcionario;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.ArrayList;
+
+public class AdicionaFuncionario {
+    public static void main(String[] args) {
+        ClienteEmpresa clienteEmpresa1, clienteEmpresa2, clienteEmpresa3;
+        Funcionario funcionario;
+        ArrayList<ClienteEmpresa> clientes = new ArrayList<>();
+
+        clienteEmpresa1 = new ClienteEmpresa("23423431", 1992, "Maria", "maria@fjpod");
+        clienteEmpresa2 = new ClienteEmpresa("8494896", 1980, "Joao", "jojo@aosj");
+        clienteEmpresa3 = new ClienteEmpresa("2135653", 1986, "Jose", "zeze@aosj");
+
+        clientes.add(clienteEmpresa1);
+        clientes.add(clienteEmpresa2);
+        clientes.add(clienteEmpresa3);
+
+        funcionario = new Funcionario();
+        funcionario.setNome("Ana");
+        funcionario.setCpf("32309873");
+        funcionario.setEmail("aninha@oiedshao");
+        funcionario.setClientes(clientes);
+
+//        System.out.println("--------------------");
+//        TransactionController.transactionAdd(funcionario);
+
+        System.out.println("--------------------");
+        TransactionController.transactionGet("32309873");
+    }
+}
---
+++ aulaPratica07JPAHibernate/src/main/java/controllers/TransactionController.java
@@ -0,0 +1,30 @@
+package controllers;
+
+import modelo2.Funcionario;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public abstract class TransactionController {
+    public static void transactionAdd(Object obj){
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("jpa_exemplo");
+        EntityManager manager = factory.createEntityManager();
+
+        manager.getTransaction().begin();
+        manager.persist(obj);
+        manager.getTransaction().commit();
+        System.out.println("Adicionando funcionário");
+        manager.close();
+    }
+
+    public static Funcionario transactionGet(String cpf){
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("jpa_exemplo");
+        EntityManager manager = factory.createEntityManager();
+
+        Funcionario encontrado = manager.find(Funcionario.class, cpf);
+        System.out.println("Buscando funcionário ->>>" + encontrado.toString());
+        manager.close();
+        return encontrado;
+    }
+}
