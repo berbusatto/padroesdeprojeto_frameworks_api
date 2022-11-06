f9f1f50db39d2f21544b1844988ccfcbdeac29bc
Bernardo Busatto <berbusatto@gmail.com>
2022-11-05 23:19:25 -0300

    adicionando laço while - ainda não funciona

--- aulaPratica07JPAHibernate/src/main/java/JPA/AdicionaCliente.java
+++ aulaPratica07JPAHibernate/src/main/java/JPA/AdicionaCliente.java
@@ -1,5 +1,6 @@
 package JPA;
 
+import controllers.TransactionController;
 import modelo2.Cliente;
 import modelo2.Conta;
 
@@ -10,39 +11,41 @@ import java.util.Scanner;
 
 public class AdicionaCliente {
     public static void main(String[] args) {
-
-
-        Cliente cliente1 = new Cliente();
-
         Scanner teclado = new Scanner(System.in);
-
-        System.out.println("Digite o nome do cliente: ");
-        cliente1.setNome(teclado.nextLine());
-
-        System.out.println("Digite o CPF do cliente: ");
-        cliente1.setCpf(teclado.nextLine());
-
-        System.out.println("Digite o email do cliente: ");
-        cliente1.setEmail(teclado.nextLine());
-
-        System.out.println("Digite o ano de nascimento do cliente: ");
-        cliente1.setAnoNascimento(teclado.nextInt());
-
-        Conta conta1 = new Conta(2000.99f);
-
-        cliente1.setConta(conta1);
-
-        EntityManagerFactory factory = Persistence.createEntityManagerFactory("jpa_exemplo");
-        EntityManager manager = factory.createEntityManager();
-
-        manager.getTransaction().begin();
-        manager.persist(cliente1);
-        manager.getTransaction().commit();
-
-        System.out.println("ID tarefa: "+ cliente1.getCpf());
-        System.out.println("Cliente adicionado");
-        manager.close();
+        String response;
 
 
+        do{
+            response = "";
+            System.out.println("Deseja cadastrar cliente? S ou N ");
+            response = teclado.nextLine();
+
+
+            if (response.equalsIgnoreCase("s")){
+                Cliente cliente1 = new Cliente();
+
+                System.out.println("Digite o nome do cliente: ");
+                cliente1.setNome(teclado.nextLine());
+
+                System.out.println("Digite o CPF do cliente: ");
+                cliente1.setCpf(teclado.nextLine());
+
+                System.out.println("Digite o email do cliente: ");
+                cliente1.setEmail(teclado.nextLine());
+
+                System.out.println("Digite o ano de nascimento do cliente: ");
+                cliente1.setAnoNascimento(teclado.nextInt());
+
+                Conta conta1 = new Conta(2000.99f);
+                cliente1.setConta(conta1);
+
+                System.out.println("--------------------");
+                TransactionController.transactionAdd(cliente1);
+
+                System.out.println("ID tarefa: "+ cliente1.getCpf());
+                System.out.println("Cliente adicionado");
+
+            }
+        } while (response.equals("s"));
     }
 }
