88a2ef00d23a2c01f8f7c34f6d2b3e02402185c5
Bernardo Busatto <berbusatto@gmail.com>
2022-11-18 21:46:33 -0300

    java spring

---
+++ aula08API/src/main/java/pr/senac/br/aula08API/Aula08ApiApplication.java
@@ -0,0 +1,13 @@
+package pr.senac.br.aula08API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aula08ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aula08ApiApplication.class, args);
+	}
+
+}
---
+++ aula08API/src/test/java/pr/senac/br/aula08API/Aula08ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pr.senac.br.aula08API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aula08ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
